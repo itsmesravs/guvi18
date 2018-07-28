75043e886afe5c6d757a9aa9a76b2fd5ff392967
itsmesravs <41770057+itsmesravs@users.noreply.github.com>
2018-07-28 21:33:51 +0530

    Create leapyear.py

---
+++ leapyear.py
@@ -0,0 +1,12 @@
+year=2016
+if(year%4==0):
+   if(year%100==0):
+       if(year%400==0):
+       print("yes")
+       else:
+       print("no")
+   else:
+     print("yes")
+else:
+     print('no")
+     
